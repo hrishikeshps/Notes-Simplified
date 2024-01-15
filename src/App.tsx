528b27e4c18a180009d8909ec64b3ffb54881c72
Hrishikesh <hrishikeshps@gmail.com>
2024-01-16 00:29:52 +0530

    routing add

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <Routes>
       <Route path='/' element={<h1>Hi! How are you?</h1>} />
       <Route path='/new' element={<h1>Hey React!</h1>} />
+      <Route path='*' element={<Navigate to="/"/>} />
+      <Route path='/:id'>
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
     </Routes>
     </>
   )
