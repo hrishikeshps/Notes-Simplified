bb7951398592f3da2e19507ebee4589aee4bbe02
Hrishikesh <hrishikeshps@gmail.com>
2024-01-17 20:07:37 +0530

    changes to the form

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,4 +1,4 @@
-import { Col, Form, Row, Stack } from "react-bootstrap";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import CreatableReactSelect from "react-select/creatable";
 
 export function NoteForm() {
@@ -18,18 +18,16 @@ export function NoteForm() {
                             <CreatableReactSelect isMulti />
                         </Form.Group>
                     </Col>
-                    <Row>
-                        <Col>
-                            <Form.Group controlId="title">
-                                <Form.Label>Note Body</Form.Label>
-                                <Form.Control required />
-                            </Form.Group>
-                        </Col>
-                    </Row>
-
                 </Row>
+                <Form.Group controlId="markdown">
+                    <Form.Label>Note Body</Form.Label>
+                    <Form.Control required as="textarea" rows={10} />
+                </Form.Group>
+                <Stack direction="horizontal" gap={2} className="justify-content-end">
+                    <Button type="submit" variant="primary">Save</Button>
+                    <Button type="button" variant="outline-secondary">Cancel</Button>
+                </Stack>
             </Stack>
-
         </Form>
     )
 }
