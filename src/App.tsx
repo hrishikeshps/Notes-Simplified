35d7e35aa0b73c5da8a4254c51986eb8df3ea108
Hrishikesh <hrishikeshps@gmail.com>
2024-06-05 23:26:22 +0530

    changes to notes structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Container } from 'react-bootstrap'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { NewNote } from './components/NewNote'
 import { useLocalStorage } from './service/useLocalStorage'
+import { useMemo } from 'react'
+import { v4 as uuidV4 } from 'uuid'
 
 export type Note = {
   id: string
@@ -11,7 +13,7 @@ export type Note = {
 
 export type RawNote = {
   id: string
-}
+} & RawNoteData
 
 export type RawNoteData = {
   title: string
@@ -33,7 +35,19 @@ export type Tag = {
 function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
-  
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return [...prevNotes, { ...data, id: uuidV4() }]
+    })
+  }
+
   return (
     <Container className="my-4">
       <Routes>
@@ -50,3 +64,4 @@ function App() {
 }
 
 export default App
+0
