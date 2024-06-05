7b5b5f0faccffef3ba019f488090d1d047426989
Hrishikesh <hrishikeshps@gmail.com>
2024-06-06 00:06:13 +0530

    list init commit

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,3 +1,23 @@
-export function NoteList(){
-    return <h1>List</h1>
+import { Button, Col, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export function NoteList() {
+    return (
+        <>
+            <Row>
+                <Col><h1>List</h1></Col>
+                <Col xs="auto">
+                    <Stack gap={2} direction="horizontal">
+                        <Link to="/new">
+                            <Button variant="primary">Create</Button>
+                        </Link>
+                        <Link to="/">
+                            <Button variant="outline-secondary">Edit Tags</Button>
+                        </Link>
+                    </Stack>
+                </Col>
+            </Row>
+        </>
+    )
+
 }
