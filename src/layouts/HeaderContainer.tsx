3ab5d8d8bd272d56559856a431260dace564e1c4
Hrishikesh <hrishikeshps@gmail.com>
2024-06-10 21:48:01 +0530

    changes to notes app

--- src/layouts/HeaderContainer.tsx
+++ src/layouts/HeaderContainer.tsx
@@ -4,11 +4,11 @@ export function HeaderContainer(){
     return (
         <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">Note-S-implified</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            {/* <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
