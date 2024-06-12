5c19b093b94b46a05b5b668391298a3e7c8466db
Hrishikesh <hrishikeshps@gmail.com>
2024-06-12 22:58:09 +0530

    changes to container

--- src/layouts/HeaderContainer.tsx
+++ src/layouts/HeaderContainer.tsx
@@ -21,17 +21,6 @@ export function HeaderContainer(){
     <Nav className="me-auto">
       <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
-      {/* <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">
-          Another action
-        </NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">
-          Separated link
-        </NavDropdown.Item>
-      </NavDropdown> */}
     </Nav>
     <Nav>
       <Nav.Link href="#login">Login</Nav.Link>
