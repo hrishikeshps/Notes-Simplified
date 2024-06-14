a17d9f39a297808f3c1480eb8916d1e8a1dffbc7
Hrishikesh <hrishikeshps@gmail.com>
2024-06-14 23:40:09 +0530

    cleanup component

--- src/layouts/HeaderContainer.tsx
+++ src/layouts/HeaderContainer.tsx
@@ -1,32 +1,21 @@
-import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
-export function HeaderContainer(){
-    return (
-      //   <Navbar bg="light" data-bs-theme="light">
-      //   <Container>
-      //     <Navbar.Brand href="#home">Note-S-implified</Navbar.Brand>
-      //     <Nav className="me-auto">
-      //       <Nav.Link href="#home">Home</Nav.Link>
-      //       {/* <Nav.Link href="#features">Features</Nav.Link>
-      //       <Nav.Link href="#pricing">Pricing</Nav.Link> */}
-      //     </Nav>
-      //   </Container>
-      // </Navbar>
-
-<Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-<Container>
-  <Navbar.Brand href="#home">Note-S-implified</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
-    </Nav>
-    <Nav>
-      <Nav.Link href="#login">Login</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Container>
-</Navbar>
-    )
+export function HeaderContainer() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Note-S-implified</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            {/* <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+          </Nav>
+          <Nav>
+            <Nav.Link href="#login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
